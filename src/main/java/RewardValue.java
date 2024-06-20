c8950f4c60ded75b47eda758ae1e66573104db7f
mu-10 <90500924+mu-10@users.noreply.github.com>
2024-06-20 11:29:18 +0200

    reward-value conversion updated

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,19 @@
+public class RewardValue {
+    private double Value;
+
+    public RewardValue(double cashValue){
+        this.Value = cashValue;
+    }
+    public RewardValue(int miles){
+        this.Value = miles * 0.0035;
+    }
+public double getValue() {
+        return Value;
+}
+public double getCashValue(){
+        return Value;
+}
+public int getMilesValue(){
+    return (int) Math.round(Value / 0.0035);
+}
+}
